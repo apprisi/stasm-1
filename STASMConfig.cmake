2438b47445610a884c5db4e9aa50d1707748d02f
melevir <melevir@gmail.com>
2014-08-23 10:17:20 +0400

    juan-cardelino's build scripts added (thanks! Original repo: https://github.com/juan-cardelino/stasm)

---
+++ STASMConfig.cmake
@@ -0,0 +1,10 @@
+SET(STASM_BASE_DIR "/Users/melevir/projects/stasm")
+
+SET(STASM_INCLUDE_DIRS "/Users/melevir/projects/stasm/stasm;/Users/melevir/projects/stasm/stasm/MOD_1")
+
+SET(STASM_LINK_DIRS ${STASM_BASE_DIR})
+
+SET(STASM_LIBRARIES stasm m)
+
+SET(STASM_FOUND 1)
+
